55aaa5596ba00a059a8aa82b9c002cd42ca82a88
josue173 <josue.perez04@outlook.com>
2022-09-22 08:17:00 -0600

    Debounce

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onQueryChange(query: string = '') {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar este query', query);
+    }, 350);
   }
-
 }
